ab2c78a1ceb44785667b5b2669ba5dcd3b85e3c2
TheShahriyar <shahriyar.31dec@gmail.com>
2023-08-19 13:31:03 +0600

    search design added

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 import "./style.scss";
+
+// Open Search Bar
+document
+  .querySelector(".search-wrapper .search-btn")
+  .addEventListener("click", () => {
+    document.querySelector(".nav-bottom").classList.add("search-open");
+  });
+
+// Close Search Bar
+document
+  .querySelector(".search-wrapper .close-icon")
+  .addEventListener("click", () => {
+    document.querySelector(".nav-bottom").classList.remove("search-open");
+  });
